9f17b1b31b2e60d6b297d657a1cf1a0616df5d00
Aditya Dewangan <adityadewangan845@gmail.com>
2026-07-04 17:57:38 +0530

    Time: 27 ms (9.73%), Space: 21.8 MB (79.65%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        n = len(nums)
+        count = 0
+        currMax = 0
+        for i in range(n):
+            if nums[i] == 1:
+                count += 1
+                currMax = max(currMax,count)
+            else:
+                count = 0
+
+        return currMax
