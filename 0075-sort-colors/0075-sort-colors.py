91b670ab66b13c6334a655ebf4fed4345babbca1
Aditya Dewangan <adityadewangan845@gmail.com>
2026-07-05 13:03:58 +0530

    Time: 0 ms (100%), Space: 19.2 MB (60.3%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,10 +3,21 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # we're gonna use the dutch-national-flag algorithm to sort 3 sections
         n = len(nums)
-        for i in range(n-1):
-            for j in range(i+1,n):
-                if nums[j] < nums[i]:
-                    nums[i],nums[j] = nums[j],nums[i]
+        low = 0
+        mid = 0
+        high = n-1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[low],nums[mid] = nums[mid],nums[low]
+                low += 1
+                mid += 1
+            elif nums[mid] == 1:
+                mid += 1
+            elif nums[mid] == 2:
+                nums[mid],nums[high] = nums[high],nums[mid]
+                high -= 1
 
         
