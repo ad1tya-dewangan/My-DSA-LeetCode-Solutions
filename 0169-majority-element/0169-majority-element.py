b4f19779059d3e5c2cdcecbe8b822626c26e5c5d
Aditya Dewangan <adityadewangan845@gmail.com>
2026-07-06 13:08:10 +0530

    Time: 0 ms (100%), Space: 20.9 MB (99.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -2,12 +2,29 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         n = len(nums)
         half = n//2
-        Map = {}    # number : frequency
+        # Using HashMap tc : O(n) sc : O(n)
+        # Map = {}    # number : frequency
+        # for num in nums:
+        #     Map[num] = Map.get(num,0) + 1
+            
+        #     if Map[num] > half:
+        #         return num
+
+        # return 0
+
+        # Using Boyer-Moore-Voting Algo tc : O(n) sc : O(1)
+        count = 0
+        candidate = 0
 
         for num in nums:
-            Map[num] = Map.get(num,0) + 1
-            
-            if Map[num] > half:
-                return num
+            if count == 0:
+                candidate = num
 
-        return None
+            if candidate == num:
+                count += 1
+            else:
+                count -= 1
+
+
+        return candidate
+            
