8105855b6e5982b7ae88e0455f9c8e1cf232e5ca
Aditya Dewangan <adityadewangan845@gmail.com>
2026-07-14 11:26:09 +0530

    Time: 0 ms (100%), Space: 19.3 MB (68.77%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,13 +1,16 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         res = []
+        # initalizing the triangle by filling it with 1s
         for i in range(numRows):
             res.append([1] * (i+1))
 
-        for i in range(1,numRows):
+        # starting from 3rd row compute only the middle values
+        for i in range(2,numRows):
             for j in range(1,i):
                 res[i][j] = res[i-1][j-1] + res[i-1][j]
 
         return res
+        
 
         
