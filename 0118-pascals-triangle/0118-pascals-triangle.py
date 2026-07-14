87089e333942fbd3f94f875322ed5116cf607860
Aditya Dewangan <adityadewangan845@gmail.com>
2026-07-14 10:55:49 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.74%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -7,7 +7,7 @@ class Solution:
         if len(res) == 1 or len(res) == 2:
             return res
 
-        for i in range(2,numRows):
+        for i in range(1,numRows):
             for j in range(1,i):
                 res[i][j] = res[i-1][j-1] + res[i-1][j]
 
