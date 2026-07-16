5186beff2f4f364a9042b0932290454ed38eb7d6
Aditya Dewangan <adityadewangan845@gmail.com>
2026-07-16 12:07:53 +0530

    Time: 3 ms (80.23%), Space: 20.5 MB (75.3%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -5,13 +5,11 @@ class Solution:
         r = n-1
         while l < r:
             total = numbers[l] + numbers[r]
-            if total < target:
+            if total == target:
+                return [l+1,r+1]
+            elif total < target:
                 l += 1
             elif total > target:
                 r -= 1
-            elif total == target:
-                return [l+1,r+1]
-                l += 1
-                r -= 1
 
-        return 0
+        return []
