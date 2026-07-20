020f0daeeb07fc1148523a94ed0605ad5671d41b
Aditya Dewangan <adityadewangan845@gmail.com>
2026-07-20 12:27:22 +0530

    Time: 11 ms (24.82%), Space: 23.4 MB (7.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -1,3 +1,10 @@
+# Sort intervals by their start time so overlapping intervals become adjacent.
+# Add the first interval to the result.
+# Compare each remaining interval with the last merged interval.
+# If the current interval starts before the last merged interval ends,
+# merge them by extending the end.
+# Otherwise, add the current interval as a new merged interval.
+
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         n = len(intervals)
@@ -6,8 +13,8 @@ class Solution:
         res.append(intervals[0])
 
         for i in range(1,n):
-            s = intervals[i][0]
-            e = intervals[i][1]
+            s = intervals[i][0]  #start interval
+            e = intervals[i][1]  #end interval
 
             last_end = res[-1][1] #res[-1][1] is last end interval in res
 
