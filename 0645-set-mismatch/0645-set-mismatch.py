0926437bea2540b3af34d88e66b463b6b22983d6
Aditya Dewangan <adityadewangan845@gmail.com>
2026-07-22 01:15:15 +0530

    Time: 6 ms (88.12%), Space: 20.3 MB (95.97%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,22 +1,35 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        hash_arr = [0]*(n+1)
+        # actual sum and square sum of numbers in array
+        S = 0
+        S2 = 0
 
         for num in nums:
-            hash_arr[num] += 1
+            S += num
+            S2 += num * num 
 
-        missing = -1
-        repeating = -1
+        # expected sum and square sum of n number
+        Sn = (n*(n+1)) // 2 
+        S2n = (n*(n+1)*(2*n+1)) // 6
 
-        for i in range(1,n+1):
-            if hash_arr[i] == 2:
-                repeating = i
-            elif hash_arr[i] == 0:
-                missing = i
+        # Suppose x is repeating and y is missing
+        val1 = S - Sn    # x - y   --> eq.(1)
+        val2 = S2 - S2n 
 
-            if missing != -1 and repeating != -1:
-                break
+        val2 = val2 // val1  # x + y   --> eq.(2)
+
+        #Add eq. (1) & (2) and we get x = val1 + val2 // 2
+        repeating = (val1 + val2) // 2
+        
+        # from eq.(1) y = x - val1
+        missing = repeating - val1
 
         return [repeating,missing]
-            
+
+
+
+
+
+
+        
